485562cb928ae0c37af96d876fcfb3764adc3018
SebSCam <sebas9903@outlook.com>
2021-08-17 15:43:51 -0500

    Script correguido y vaciado de log

--- middleware/resources/script.sh
+++ middleware/resources/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -r SERVER_HOST_IP=('localhost:3001/status' '172.217.15.206')
+declare -r SERVER_HOST_IP=('localhost' '172.217.15.206')
 
 while :
 do
@@ -9,12 +9,12 @@ for i in "${SERVER_HOST_IP[@]}"
         curl http://${i} -w ", %{http_code}"
         if [ $? -eq 0 ]
         then
-        STATE="$(curl http://${i}:3000 -w#%{http_code})"
+        STATE="$(curl http://${i}:3000 -w"#%{http_code}")"
         sFileDate="$(date +"%d/%m/%y#%H:%M:%S#${i}#online")#$STATE"
         echo $sFileDate>>info.log
         else 
         date +"%d/%m/%y#%H:%M:%S#${i}#offline">>info.log
         fi
-        
-sleep 2m
+done
+sleep 2 
 done
