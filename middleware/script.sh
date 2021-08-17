ecf7cfada7c64d30b74f120bde1f4f4799f76eb2
SebSCam <sebas9903@outlook.com>
2021-08-17 15:45:06 -0500

    script externo

--- middleware/script.sh
+++ middleware/script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+declare -r SERVER_HOST_IP=('localhost' '172.217.15.206')
 
 while :
 do
-declare -r SERVER_HOST_IP=('192.168.0.111' '172.217.15.206')
+
 for i in "${SERVER_HOST_IP[@]}"
-    do
-        curl http://${i}:3002 -w ", %{http_code}"
+    do        
+        curl http://${i} -w ", %{http_code}"
         if [ $? -eq 0 ]
         then
-        STATE="$(curl http://${i}:3000 -w#%{http_code})"
+        STATE="$(curl http://${i}:3000 -w"#%{http_code}")"
         sFileDate="$(date +"%d/%m/%y#%H:%M:%S#${i}#online")#$STATE"
         echo $sFileDate>>info.log
         else 
@@ -17,4 +18,3 @@ for i in "${SERVER_HOST_IP[@]}"
 done
 sleep 2 
 done
-
