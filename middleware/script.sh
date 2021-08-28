3b03957c642d1773c7606eddfc135f7bd9f68be8
SebSCam <sebas9903@outlook.com>
2021-08-28 03:55:18 -0500

    log script

--- middleware/script.sh
+++ middleware/script.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 declare -r SERVER_HOST_IP=('192.168.100.140' '192.168.100.160')
-
 while :
 do
-
-for i in "${SERVER_HOST_IP[@]}"
-    do        
+    for i in "${SERVER_HOST_IP[@]}"
+    do 
         curl http://${i}:3000 -w ", %{http_code}"
         if [ $? -eq 0 ]
         then
-        STATE="$(curl http://${i}:3000 -w"#%{http_code}")"
-        sFileDate="$(date +"%d/%m/%y#%H:%M:%S#${i}#online")#$STATE"
-        echo $sFileDate>>info.log
-        else 
-        date +"%d/%m/%y#%H:%M:%S#${i}#offline">>info.log
+            STATE="$(curl http://${i}:3000 -w"#%{http_code}")"
+            sFileDate="$(date +"%d/%m/%y#%H:%M:%S#${i}#online")#$STATE"
+            echo $sFileDate>>info.log
+        else
+            date +"%d/%m/%y#%H:%M:%S#${i}#offline">>info.log
         fi
     done
 sleep 10
